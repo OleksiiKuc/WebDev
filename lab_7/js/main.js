16fb14d8dc4dd88b198c26b54efcaef7d3d4ab3b
OleksiiKuc <74044099+OleksiiKuc@users.noreply.github.com>
2021-04-20 21:50:24 +0300

    lab_7

---
+++ lab_7/js/main.js
@@ -0,0 +1,83 @@
+function triangle(argument_1, type_1, argument_2, type_2) {
+    var a, b, c, alpha, beta;
+    type_1 = document.getElementById("type_1").value;
+    type_2 = document.getElementById("type_2").value;
+    argument_1 = document.getElementById("argument_1").value;
+    argument_2 = document.getElementById("argument_2").value;
+    if(type_1 == "leg" && type_2 == "leg") {
+        a = argument_1;
+        b = argument_2;
+        c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        alpha = Math.asin(a/c);
+        beta = Math.acos(a/c);
+    } else if (type_1 == "leg" && type_2 == "hypotenuse") {
+        a = argument_1;
+        c = argument_2;
+        b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
+        alpha = Math.atan(a/b);
+        beta = Math.atan(b/a);
+    } else if(type_1 == "leg" && type_2 == "adjacent angle") {
+        a = argument_1;
+        beta = argument_2;
+        c = a / (Math.cos(beta * Math.PI/180));
+        b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
+        alpha = (90 * Math.PI / 180) - beta;
+    } else if(type_1 == "leg" && type_2 == "opposite angle") {
+        a = argument_1;
+        alpha = argument_2;
+        c = a / (Math.sin(alpha * Math.PI/180));
+        b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
+        beta = (90 * Math.PI / 180) - alpha;
+    } else if(type_1 == "hypotenuse" && type_2 == "angle") {
+        c = argument_1;
+        beta = argument_2;
+        alpha = (90 * Math.PI / 180) - beta;
+        a = c * Math.sin(alpha * Math.PI/180);
+        b = c * Math.sin(beta * Math.PI/180);
+    } else if(type_1 == "hypotenuse" && type_2 == "leg") {
+        c = argument_1;
+        a = argument_2;
+        b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
+        alpha = Math.atan(a/b);
+        beta = Math.atan(b/a);
+    } else if(type_1 == "adjacent angle" && type_2 == "leg") {
+        beta = argument_1;
+        a = argument_2;
+        c = a / (Math.cos(beta * Math.PI/180));
+        b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
+        alpha = (90 * Math.PI / 180) - beta;
+    } else if(type_1 == "opposite angle" && type_2 == "leg") {
+        alpha = argument_1;
+        a = argument_2;
+        c = a / (Math.sin(alpha * Math.PI/180));
+        b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
+        beta = (90 * Math.PI / 180) - alpha;
+    } else if(type_1 == "angle" && type_2 == "hypotenuse") {
+        beta = argument_1;
+        c = argument_2;
+        alpha = (90 * Math.PI / 180) - beta;
+        a = c * Math.sin(alpha * Math.PI/180);
+        b = c * Math.sin(beta * Math.PI/180);
+    } else {
+        return "Zero or negative input";
+    }
+    if (argument_1 <= 0 || argument_2 <= 0) {
+        return "Zero or negative input";
+    }
+    if(type_1 == "leg" && type_2 == "hypotenuse" && argument_1 <= argument_2 || type_2 == "leg" && type_1 == "hypotenuse" && argument_2 <= argument_1) {
+        return "Zero or negative input";
+    }
+    if(argument_1 > 90 || argument_2 > 90) {
+        return "Zero or negative input";
+    }
+    console.info(argument_1, type_1, argument_2, type_2);
+    return "a = " + a + "\nb = " + b + "\nc = " + c + "\nalpha = " + alpha + "\nbeta = " + beta + "\nsuccess!";
+
+}
+
+$('.button').on('click', function () {
+    console.log(triangle());
+});
+
+
+
